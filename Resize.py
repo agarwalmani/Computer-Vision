07f270e14eec5e2d22dfbf08ba180becd804d2e0
Mani Agarwal <57814186+agarwalmani@users.noreply.github.com>
2019-11-20 12:44:10 +0530

    Add files via upload

---
+++ Resize.py
@@ -0,0 +1,18 @@
+# Program to resize a image.
+
+# Author:- Mani Agarwal
+# Email:- maniagarwal@jklu.edu.in
+
+import cv2
+
+img=cv2.imread("C:\\Users\\user\\Downloads\\Taylor1.jpg",1)   # 1 is for coloured image.
+
+img_1=cv2.imread("C:\\Users\\user\\Downloads\\Taylor1.jpg",0) # 0 is for converting the image into gray.
+
+resized_image=cv2.resize(img,(400,400))  # For resizing the image
+cv2.imshow("Taylor Swift",resized_image)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# Note :- You must write waitKey() and destroyAllWindows(), otherwise the error of "Not Responding may come".
